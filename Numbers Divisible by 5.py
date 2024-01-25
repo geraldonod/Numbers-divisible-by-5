a5d3141c80f6e4f8f20806447f968953a56dc6e4
Gerald <geraldonod@gmail.com>
2024-01-25 17:42:21 +0800

    check number list for number divisible by 5

--- Numbers Divisible by 5.py
+++ Numbers Divisible by 5.py
@@ -2,3 +2,7 @@
 num_list = [10, 20, 33, 46, 55]
 
 #check number list for numbers divisible by 5
+print("Numbers divisible by 5: ")
+for number in num_list:
+    if number % 5 == 0:
+        print (number)
